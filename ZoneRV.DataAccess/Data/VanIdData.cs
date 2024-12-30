428f6b3b66347a32769d1bed75712a5d9e30c77b
MrWiggims1 <Liamth@live.com.au>
2024-12-30 20:38:23 +1000

    Added DB access

---
+++ ZoneRV.DataAccess/Data/VanIdData.cs
@@ -0,0 +1,36 @@
+﻿using ZoneRV.DataAccess.DbAccess;
+using ZoneRV.DataAccess.Models;
+
+namespace ZoneRV.DataAccess.Data;
+
+public class VanIdData
+{
+    private readonly SqlDataAccess _db;
+
+    public VanIdData(SqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<VanID>> GetIds()
+        => _db.LoadData<VanID, dynamic>("dbo.spVanId_GetAll", new { });
+
+    public async Task<VanID?> GetId(string vanName)
+    {
+        var results = await _db.LoadData<VanID, dynamic>("dbo.spVanId_get", new { VanName = vanName });
+
+        return results.FirstOrDefault();
+    }
+
+    public async Task InsertVanId(VanID vanId)
+        => await _db.SaveData("dbo.spVanId_Insert", vanId);
+
+    public async Task UpdateVanId(VanID vanId)
+        => await _db.SaveData("dbo.spVanId_Update", vanId);
+
+    public async Task DeleteVanId(string vanName)
+        => await _db.SaveData("dbo.SpVanId_Delete", new { VanName = vanName });
+        
+    public async Task BlockVan(string vanName, bool blocked)
+        => await _db.SaveData("dbo.SpVanId_Block", new { VanName = vanName, Blocked = blocked });
+}
---
+++ ZoneRV.DataAccess/Models/VanID.cs
@@ -0,0 +1,9 @@
+﻿namespace ZoneRV.DataAccess.Models;
+
+public class VanID
+{
+    public required string VanId { get; set; }
+    public required string VanName { get; init; }
+    public bool Blocked { get; set; } = false;
+    public required string Url { get; set; }
+}
